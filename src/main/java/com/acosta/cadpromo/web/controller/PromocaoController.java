1b0c7e712d1a4b05588ddbe858e1e81868f0ac68
Alberto Costa <mihcadevs@gmail.com>
2019-06-30 22:47:11 -0300

    Submetendo o formulario e criação do metodo salvar

--- src/main/java/com/acosta/cadpromo/web/controller/PromocaoController.java
+++ src/main/java/com/acosta/cadpromo/web/controller/PromocaoController.java
@@ -1,32 +1,52 @@
 package com.acosta.cadpromo.web.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.acosta.cadpromo.domain.Categoria;
+import com.acosta.cadpromo.domain.Promocao;
 import com.acosta.cadpromo.repository.CategoriaRepository;
+import com.acosta.cadpromo.repository.PromocaoRepository;
 
 @Controller
 @RequestMapping("/promocao")
 public class PromocaoController {
-
+	
+	private static Logger log = LoggerFactory.getLogger(PromocaoController.class);
+	
+    @Autowired
+    private PromocaoRepository promocaoRepository;
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-
+	
+	@PostMapping("/save")
+	public ResponseEntity<Promocao> salvarPromocao(Promocao promocao) {
+		
+		log.info("Promocao {}", promocao.toString());
+		promocao.setDtCadastro(LocalDateTime.now());
+		promocaoRepository.save(promocao);
+		return ResponseEntity.ok().build();
+	}
+	
 	@ModelAttribute("categorias")
 	public List<Categoria> getCategorias() {
-		 return categoriaRepository.findAll();
-	 }
+		
+		return categoriaRepository.findAll(); 
+	}
 
 	@GetMapping("/add")
 	public String abrirCadastro() {
-
+		
 		return "promo-add";
 	}
-
 }
