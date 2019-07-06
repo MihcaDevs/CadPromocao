5a62f093dec149c310aae57842225a0eea3bced2
Alberto Costa <mihcadevs@gmail.com>
2019-07-06 14:38:12 -0300

    Validando Back End e AJAX

--- src/main/java/com/acosta/cadpromo/web/controller/PromocaoController.java
+++ src/main/java/com/acosta/cadpromo/web/controller/PromocaoController.java
@@ -1,13 +1,19 @@
 package com.acosta.cadpromo.web.controller;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +23,8 @@ import com.acosta.cadpromo.domain.Categoria;
 import com.acosta.cadpromo.domain.Promocao;
 import com.acosta.cadpromo.repository.CategoriaRepository;
 import com.acosta.cadpromo.repository.PromocaoRepository;
+
+
 
 @Controller
 @RequestMapping("/promocao")
@@ -30,7 +38,17 @@ public class PromocaoController {
 	private CategoriaRepository categoriaRepository;
 	
 	@PostMapping("/save")
-	public ResponseEntity<Promocao> salvarPromocao(Promocao promocao) {
+	public ResponseEntity<?> salvarPromocao(@Valid Promocao promocao, BindingResult result) {
+		
+		if (result.hasErrors()) {
+			
+			Map<String, String> errors = new HashMap<>();
+			for (FieldError error : result.getFieldErrors()) {
+				errors.put(error.getField(), error.getDefaultMessage());
+			}
+			
+			return ResponseEntity.unprocessableEntity().body(errors);
+		}
 		
 		log.info("Promocao {}", promocao.toString());
 		promocao.setDtCadastro(LocalDateTime.now());
