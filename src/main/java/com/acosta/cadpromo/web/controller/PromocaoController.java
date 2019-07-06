c199aa048d74bbb067527a777fdb3fcebb931ca5
Alberto Costa <mihcadevs@gmail.com>
2019-07-06 15:53:49 -0300

    Listando as promoçoes com AJAX

--- src/main/java/com/acosta/cadpromo/web/controller/PromocaoController.java
+++ src/main/java/com/acosta/cadpromo/web/controller/PromocaoController.java
@@ -10,14 +10,18 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.acosta.cadpromo.domain.Categoria;
 import com.acosta.cadpromo.domain.Promocao;
@@ -36,6 +40,26 @@ public class PromocaoController {
     private PromocaoRepository promocaoRepository;
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	// ======================================LISTAR OFERTAS==========================================
+	
+	@GetMapping("/list")
+	public String listarOfertas(ModelMap model) {
+		Sort sort = new Sort(Sort.Direction.DESC, "dtCadastro");
+		PageRequest pageRequest = PageRequest.of(0, 8, sort);
+		model.addAttribute("promocoes", promocaoRepository.findAll(pageRequest));
+		return "promo-list";
+	}
+	
+	@GetMapping("/list/ajax")
+	public String listarCards(@RequestParam(name = "page", defaultValue = "1") int page, ModelMap model) {
+		Sort sort = new Sort(Sort.Direction.DESC, "dtCadastro");
+		PageRequest pageRequest = PageRequest.of(page, 8, sort);
+		model.addAttribute("promocoes", promocaoRepository.findAll(pageRequest));		
+		return "promo-card";
+	}	
+	
+	// ======================================ADD OFERTAS=============================================
 	
 	@PostMapping("/save")
 	public ResponseEntity<?> salvarPromocao(@Valid Promocao promocao, BindingResult result) {
